6e55d7fde4dfdf0a267456686897db1b5d0be1cd
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2016-08-07 16:09:11 +0200

    improved function with else statement

--- IsInvertible.m
+++ IsInvertible.m
@@ -1,18 +1,12 @@
 function f = IsInvertible(a,b,c,d)
-
 % first check if the 'Determinant' is zero
 if ((a*d)-(b*c) == 0)
     disp('Matrix NOT invertible!');
-end
-
-% if not zero, display this
-if ((a*d)-(b*c) ~= 0)
-    disp('Matrix IS invertible!');
-    
+else
+    disp('Matrix IS invertible!');    
     % calculate inversed matrix
     Matrix = [d -b; -c a];
     InversedMatrix=(1/((a*d)-(b*c)))*Matrix;
-
     % return inversed matrix
     f = InversedMatrix;
 end
