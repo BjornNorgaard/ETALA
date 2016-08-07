b3d030ab93d0082de90e5af1434963665d9d0a50
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2016-08-07 17:43:31 +0200

    first part done and good, need to do the same again with other vehivles

--- Case 1/case1.m
+++ Case 1/case1.m
@@ -23,13 +23,13 @@ p2=subs(s2,x-3.1);
 p3=subs(s3,x-10.3);
 
 % returning symbolic polynomials to coefficient polynomials
-r1=poly2sym(p1);
-r2=poly2sym(p2);
-r3=poly2sym(p3);
+r1=sym2poly(p1);
+r2=sym2poly(p2);
+r3=sym2poly(p3);
 
 % graphing piecewise function
 t=[0 3.1 10.3 30.1];
-vol=[0 30 60 90];
+vel=[0 30 60 90];
 xx1=  0   : .1 :  3.1 ;   v1=polyval(r1,xx1);
 xx2=  3.1 : .1 : 10.3 ;   v2=polyval(r2,xx2);
 xx3= 10.3 : .1 : 30.1 ;   v3=polyval(r3,xx3);
@@ -38,3 +38,10 @@ hold on % does what?
 
 % graph things
 plot(t, vel,'o',xx1,v1,xx2,v2,xx3,v3);
+
+% 1. How long will it take the CR-V to accelerate to 50 mph?
+double(solve(p2-50))
+
+% 2. How must distance will it take for the CR-V to accelerate to 90mph?
+f=int(p1/3600,0,3.1)+int(p2/3600,3.1,10.3)+int(p3/3600,10.3,30.1);
+double(f)
